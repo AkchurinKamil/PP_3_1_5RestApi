2a32460a924430b9eda2850c3476216e1599a1d0
kamil <akchurinkami@Gmail.com>
2023-06-23 20:50:21 +0300

    work

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -23,7 +23,7 @@ public class User implements UserDetails {
     private int age;
     @Column(name = "password")
     private String password;
-    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
